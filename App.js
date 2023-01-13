ed2ee11f7f73f0d42de9ec4eecf41c5ed1fac9c4
Ashutosh <ashutoshkumar6341@gmail.com>
2023-01-13 10:26:07 +0530

    my first react-native-app

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomComponent from './src/screens/CustomComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const myname = "Ashutosh kumar";
+  const myelement = <Text>Hello this is Ashutosh kumar</Text>
+  const getFullName = (firstName,secondName,thirdName) =>{
+    return `My name is ${firstName} ${secondName} ${thirdName}`
+  }
+ return(
+  <View style={styles.container}>
+    <Text>Hi , {getFullName("vinod", "bahadur", "thapa")}</Text>
+    <CustomComponent />
+    {myelement}
+  </View>
+ )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container : {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    marginTop: 100,
+  }
+})
